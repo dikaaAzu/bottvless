be5058668f82839283870a0a4e2f11a003459f2a
dikaaAzu <dikaaabyan@gmail.com>
2026-09-07 16:13:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -421,7 +421,7 @@ async function generateAndSendVless(ctx) {
     const selectedDomain = session.domain;
 
     const uuid = getUuid(); 
-    const pathValue = prov.path ? prov.path : `/%2F${prov.proxy}`;
+    const pathValue = prov.path ? prov.path : `/${prov.proxy}`;
     const accountName = `${session.country}-${prov.name} ${countryData.flag}`;
     const encodedName = encodeURIComponent(accountName);
 
